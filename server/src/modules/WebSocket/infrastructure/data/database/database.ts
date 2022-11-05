c88c2da0a7d2d15d102a16c715eac4aad5b9b242
kaiquye <kaiquemendesilva82@gmail.com>
2022-11-05 16:10:10 -0300

    add: delete race

--- server/src/modules/WebSocket/infrastructure/data/database/database.ts
+++ server/src/modules/WebSocket/infrastructure/data/database/database.ts
@@ -5,8 +5,30 @@ class Database {
     return this.data.filter((data) => data.room == room).length;
   }
 
-  public saveRoom(room: string) {
-    this.data.push({ room: room });
+  public save(data) {
+    this.data.push(data);
+  }
+
+  public getRunnersByRoom(room: string) {
+    return this.data.filter((data) => data.room === room);
+  }
+
+  public update(nickname, data) {
+    const index = this.data.findIndex((value) => value.nickname === nickname);
+
+    if (index < 0) {
+      throw new Error("[db] nickname not found");
+    }
+
+    this.data[index].time = data.time;
+    this.data[index].burned = data.burned;
+    this.data[index].finished = true;
+  }
+
+  public deleteRace(room: string) {
+    const index = this.data.findIndex((value) => value.room === room);
+
+    this.data[index] = undefined;
   }
 }
 
--- server/src/modules/WebSocket/infrastructure/events/events.ts
+++ server/src/modules/WebSocket/infrastructure/events/events.ts
@@ -17,9 +17,19 @@ class EventsWebSocket {
     this.event.on("room", (data: any) => {
       console.log(data);
       this.event.join(data.room);
-      this.dbCon.saveRoom(data.room);
+      this.event.join(data.nickname);
+
+      this.dbCon.save({
+        room: data.room,
+        nickname: data.nickname,
+        burned: null,
+        finished: false,
+        time: null,
+      });
+
       this.StartGame(data.room);
     });
+    this.finishRace();
   }
 
   private StartGame(room: string): void {
@@ -30,6 +40,52 @@ class EventsWebSocket {
     if (numberOfParticipants >= 2) {
       this.con.to(room).emit("start", true);
     }
+  }
+
+  private finishRace() {
+    this.event.on(
+      "finishRace",
+      (data: {
+        time: string;
+        nickname: string;
+        burned: boolean;
+        room: string;
+      }) => {
+        this.dbCon.update(data.nickname, {
+          time: data.time,
+          burned: data.burned,
+        });
+
+        const runners = this.dbCon.getRunnersByRoom(data.room);
+
+        const player2 = runners.find(
+          (value) => value.nickname !== data.nickname && value.finished === true
+        );
+
+        if (player2) {
+          if (player2.burned) {
+            const deleted = this.dbCon.deleteRace(data.room);
+            console.log("deleted", deleted);
+            return this.con.to(data.nickname).emit("finish", "voce ganhou");
+          }
+
+          if (data.burned) {
+            const deleted = this.dbCon.deleteRace(data.room);
+            console.log("deleted", deleted);
+            return this.con.to(player2.nickname).emit("finish", "voce ganhou");
+          }
+
+          if (player2.time < data.time) {
+            const deleted = this.dbCon.deleteRace(data.room);
+            console.log("deleted", deleted);
+            return this.con.to(player2.nickname).emit("finish", "voce ganhou");
+          }
+
+          this.dbCon.deleteRace(data.room);
+          return this.con.to(data.nickname).emit("finish", "voce ganhou");
+        }
+      }
+    );
   }
 }
 
